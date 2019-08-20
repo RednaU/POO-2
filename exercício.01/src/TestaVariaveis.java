b61b95fd56d8eb3686ebb0240aae06ae42432d4a
RednaU <53955744+RednaU@users.noreply.github.com>
2019-08-19 21:16:05 -0300

    Add files via upload

---
+++ exercício.01/src/TestaVariaveis.java
@@ -0,0 +1,23 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		String nome = "Luana";
+		System.out.println(nome);
+		
+		int idade = 30;
+		System.out.println(idade);
+		
+		idade += (7 * 5 + 2);
+		System.out.println(idade);
+		
+		System .out.println(nome + " tem " + idade + " anos ");
+		
+		double salario = 1270.50;
+		long valor = 123456000000l;
+		float salario2 = 3.14f;
+		
+
+	}
+
+}
